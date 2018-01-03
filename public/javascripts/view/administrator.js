2f62958e66722702246d0bd0d5fa181296110f28
ZhangQiang <Johnny$0627>
2018-01-03 22:57:26 +0800

    管理员信息维护已完成

--- public/javascripts/view/administrator.js
+++ public/javascripts/view/administrator.js
@@ -1,130 +1,92 @@
 var app = new Vue({
-    el: '#app',
-    data: {
-        administratorList:[],
-        administratorID: '',
-        administratorName: '',
-        selectedCustomerRole:'',
-        selectedStatus:'',
-        account: '',
-        cellphone: '',
-        email: '',
-        customerRoleList: [
-            {'key': 'S','value':'最高权限'},
-            {'key': 'C','value':'修改权限'},
-            {'key': 'Q','value':'查询权限'}
-            ],
-        statusList: [
-            {'key': 'P','value':'待审核'},
-            {'key': 'A','value':'审核通过'},
-            {'key': 'N','value':'审核未通过'},
-            {'key': 'F','value':'冻结'}
-        ],
-        administratorNameValid: false,
-        saveType: ''
-    },
-    computed: {
-        enabledSave: function () {
-            return this.administratorID.length > 0
-                && this.administratorName.length > 0
-                && this.selectedCustomerRole.length > 0
-                && this.selectedStatus.length > 0
-                && this.account.length > 0
-                && this.cellphone.length > 0
-                && this.email.length > 0;
-                // && this.brandNameENValid > 0;
-        }
-    },
-    methods:{
-        onChange: function (rowIndex) {
-            var row = $('#administrator-list tbody tr').eq(rowIndex);
-            app.$data.administratorID = $(row).find('td').eq(0).text();
-            app.$data.administratorName = $(row).find('td').eq(1).text();
-            app.$data.account = $(row).find('td').eq(2).text();
-            app.$data.cellphone = $(row).find('td').eq(3).text();
-            app.$data.email = $(row).find('td').eq(4).text();
-            app.$data.selectedCustomerRole = $(row).find('td').eq(5).text();
-            app.$data.selectedStatus = $(row).find('td').eq(6).text();
-            app.$data.saveType = 'change';
-            hiddenMessage();
-            $('#myModal').modal('show');
-        },
-        onDelete: function (administratorID, administratorName) {
-            var confirmMsg = '您确定要删除' + administratorName + '吗？';
-            bootbox.confirm(confirmMsg, function(result) {
-                if(result) {
-                    $.ajax({
-                        url: '/administrator?administratorID=' + administratorID,
-                        type: 'delete',
-                        dataType: 'json',
-                        success: function(res){
-                            if(res.err){
-                                alertMessage(res.msg);
-                            }else{
-                                location.reload();
-                            }
-                        },
-                        error: function(XMLHttpRequest){
-                            alertMessage('远程服务无响应，状态码：' + XMLHttpRequest.status);
-                        }
-                    });
-                }
-            });
-        },
-        onSave: function () {
-            var dataType = '';
-            var saveData = null;
-            if(app.$data.saveType === ''){
-                return false;
-            }
-            if(app.$data.saveType === 'add'){
-                dataType = 'post';
-                saveData = {
-                    administratorID: app.$data.selectedAdministrator,
-                    administratorName: app.$data.administratorName,
-                    loginUser: getLoginUser()
-                };
-            }else{
-                dataType = 'put';
-                saveData = {
-                    administratorID: app.$data.administratorID,
-                    administratorName: app.$data.administratorName,
-                    loginUser: getLoginUser()
-                };
-            }
-            $.ajax({
-                url: '/administrator',
-                type: dataType,
-                dataType: 'json',
-                data:saveData,
-                success: function(res){
-                    if(res.err){
-                        showMessage(res.msg);
-                    }else{
-                        location.reload();
-                    }
-                },
-                error: function(XMLHttpRequest, textStatus){
-                    showMessage('远程服务无响应，状态码：' + XMLHttpRequest.status);
-                }
-            });
-        }
-    },
-    mounted: function () {
-        $.ajax({
-            url: '/administrator/administratorList',
-            type: 'GET',
-            success: function(res){
-                if(res.err){
-                    alertMessage(res.msg);
-                    //showMessage(res.msg);
-                }else{
-                    app.$data.administratorList = res.administratorList
-                }
-            },
-            error: function(XMLHttpRequest, textStatus){
-                showMessage('远程服务无响应，状态码：' + XMLHttpRequest.status);
-            }
-        });
+  el: '#app',
+  data: {
+    administratorList:[],
+    administratorID: '',
+    administratorName: '',
+    selectedCustomerRole:'',
+    selectedStatus:'',
+    account: '',
+    cellphone: '',
+    email: '',
+    customerRoleList: [
+      {'key': 'S','value':'最高权限'},
+      {'key': 'C','value':'修改权限'},
+      {'key': 'Q','value':'查询权限'}
+    ],
+    statusList: [
+      {'key': 'P','value':'待审核'},
+      {'key': 'A','value':'审核通过'},
+      {'key': 'N','value':'审核未通过'},
+      {'key': 'F','value':'冻结'}
+    ],
+    saveType: ''
+  },
+  computed: {
+    enabledSave: function () {
+      return this.selectedCustomerRole.length > 0
+          && this.selectedStatus.length > 0;
     }
+  },
+  methods:{
+    onChange: function (rowIndex) {
+      var row = $('#administrator-list tbody tr').eq(rowIndex);
+      app.$data.administratorID = $(row).find('td').eq(0).text();
+      app.$data.administratorName = $(row).find('td').eq(1).text();
+      app.$data.account = $(row).find('td').eq(2).text();
+      app.$data.cellphone = $(row).find('td').eq(3).text();
+      app.$data.email = $(row).find('td').eq(4).text();
+      app.$data.selectedCustomerRole = $(row).find('td').eq(5).find('input').val();
+      app.$data.selectedStatus = $(row).find('td').eq(6).find('input').val();
+      app.$data.saveType = 'change';
+      hiddenMessage();
+      $('#myModal').modal('show');
+    },
+    onDelete: function (administratorID, administratorName) {
+      var confirmMsg = '您确定要删除' + administratorName + '吗？';
+      bootbox.confirm(confirmMsg, function(result) {
+        if(result) {
+          $.ajax({
+            url: '/administrator?administratorID=' + administratorID,
+            type: 'delete',
+            dataType: 'json',
+            success: function(res){
+              if(res.err){
+                alertMessage(res.msg);
+              }else{
+                location.reload();
+              }
+            },
+            error: function(XMLHttpRequest){
+              alertMessage('远程服务无响应，状态码：' + XMLHttpRequest.status);
+            }
+          });
+        }
+      });
+    },
+    onSave: function () {
+      var saveData = {
+        administratorID: app.$data.administratorID,
+        customerRole: app.$data.selectedCustomerRole,
+        status: app.$data.selectedStatus,
+        loginUser: getLoginUser()
+      };
+      $.ajax({
+        url: '/administrator',
+        type: 'put',
+        dataType: 'json',
+        data:saveData,
+        success: function(res){
+          if(res.err){
+            showMessage(res.msg);
+          }else{
+            location.reload();
+          }
+        },
+        error: function(XMLHttpRequest, textStatus){
+          showMessage('远程服务无响应，状态码：' + XMLHttpRequest.status);
+        }
+      });
+    }
+  }
 });
--- routes/administrator.js
+++ routes/administrator.js
@@ -4,208 +4,204 @@ var pagingUtils = require('../common/pagingUtils');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-    var service = new commonService.CommonService('administrator');
-    var pageNumber = req.query.page;
-    if(pageNumber === undefined){
-        pageNumber = 1;
-    }
+  var service = new commonService.CommonService('administrator');
+  var pageNumber = req.query.page;
+  if(pageNumber === undefined){
+    pageNumber = 1;
+  }
 
-    service.getAll(pageNumber, function (result) {
-        if(result.err || !result.content.result){
-            res.render('administrator', {
-                title: '管理员信息维护',
-                totalCount: 0,
-                paginationArray:[],
-                administratorList: []
-            });
-        }else{
-            var paginationArray = pagingUtils.getPaginationArray(pageNumber, result.content.totalCount);
-            var prePaginationNum = pagingUtils.getPrePaginationNum(pageNumber);
-            var nextPaginationNum = pagingUtils.getNextPaginationNum(pageNumber, result.content.totalCount);
-            var renderData = {};
-            if(result.content.responseData === null){
-                renderData = {
-                    title: '管理员信息维护',
-                    totalCount: result.content.totalCount,
-                    currentPageNum: pageNumber,
-                    administratorList: result.content.responseData
-                }
-            }else{
-                if(prePaginationNum > 0 && nextPaginationNum > 0){
-                    renderData = {
-                        title: '管理员信息维护',
-                        totalCount: result.content.totalCount,
-                        paginationArray: paginationArray,
-                        prePageNum: prePaginationNum,
-                        nextPageNum: nextPaginationNum,
-                        currentPageNum: pageNumber,
-                        administratorList: result.content.responseData
-                    }
-                }else if(prePaginationNum === 0){
-                    renderData = {
-                        title: '管理员信息维护',
-                        totalCount: result.content.totalCount,
-                        paginationArray: paginationArray,
-                        nextPageNum: nextPaginationNum,
-                        currentPageNum: pageNumber,
-                        administratorList: result.content.responseData
-                    }
-                }else {
-                    renderData = {
-                        title: '管理员信息维护',
-                        totalCount: result.content.totalCount,
-                        paginationArray: paginationArray,
-                        prePageNum: prePaginationNum,
-                        currentPageNum: pageNumber,
-                        administratorList: result.content.responseData
-                    }
-                }
-            }
-
-            res.render('administrator', renderData);
+  service.getAll(pageNumber, function (result) {
+    if(result.err || !result.content.result){
+      res.render('administrator', {
+        title: '管理员信息维护',
+        totalCount: 0,
+        paginationArray:[],
+        administratorList: []
+      });
+    }else{
+      var paginationArray = pagingUtils.getPaginationArray(pageNumber, result.content.totalCount);
+      var prePaginationNum = pagingUtils.getPrePaginationNum(pageNumber);
+      var nextPaginationNum = pagingUtils.getNextPaginationNum(pageNumber, result.content.totalCount);
+      var renderData = {};
+      if(result.content.responseData === null){
+        renderData = {
+          title: '管理员信息维护',
+          totalCount: result.content.totalCount,
+          currentPageNum: pageNumber,
+          administratorList: result.content.responseData
         }
-    });
+      }else{
+        if(prePaginationNum > 0 && nextPaginationNum > 0){
+          renderData = {
+            title: '管理员信息维护',
+            totalCount: result.content.totalCount,
+            paginationArray: paginationArray,
+            prePageNum: prePaginationNum,
+            nextPageNum: nextPaginationNum,
+            currentPageNum: pageNumber,
+            administratorList: result.content.responseData
+          }
+        }else if(prePaginationNum === 0){
+          renderData = {
+            title: '管理员信息维护',
+            totalCount: result.content.totalCount,
+            paginationArray: paginationArray,
+            nextPageNum: nextPaginationNum,
+            currentPageNum: pageNumber,
+            administratorList: result.content.responseData
+          }
+        }else {
+          renderData = {
+            title: '管理员信息维护',
+            totalCount: result.content.totalCount,
+            paginationArray: paginationArray,
+            prePageNum: prePaginationNum,
+            currentPageNum: pageNumber,
+            administratorList: result.content.responseData
+          }
+        }
+      }
+
+      res.render('administrator', renderData);
+    }
+  });
 });
 
 router.get('/customerRoleList', function (req, res, next) {
-    var service = new commonService.CommonService('administrator');
-    var pageNumber = req.query.page;
-    if(pageNumber === undefined){
-        pageNumber = 1;
-    }
+  var service = new commonService.CommonService('administrator');
+  var pageNumber = req.query.page;
+  if(pageNumber === undefined){
+    pageNumber = 1;
+  }
 
-    service.getAll(pageNumber, function (result) {
-        if(result.err || !result.content.result){
-            res.json({
-                err: true,
-                msg: result.msg
-            });
-        }else{
-            res.json({
-                err: false,
-                msg: result.content.responseMessage,
-                countryList: result.content.responseData
-            });
-        }
-    });
+  service.getAll(pageNumber, function (result) {
+    if(result.err || !result.content.result){
+      res.json({
+        err: true,
+        msg: result.msg
+      });
+    }else{
+      res.json({
+        err: false,
+        msg: result.content.responseMessage,
+        customerRoleList: result.content.responseData
+      });
+    }
+  });
 });
 
 router.get('/statusList', function (req, res, next) {
-    var service = new commonService.CommonService('administrator');
-    var pageNumber = req.query.page;
-    if(pageNumber === undefined){
-        pageNumber = 1;
-    }
+  var service = new commonService.CommonService('administrator');
+  var pageNumber = req.query.page;
+  if(pageNumber === undefined){
+    pageNumber = 1;
+  }
 
-    service.getAll(pageNumber, function (result) {
-        if(result.err || !result.content.result){
-            res.json({
-                err: true,
-                msg: result.msg
-            });
-        }else{
-            res.json({
-                err: false,
-                msg: result.content.responseMessage,
-                countryList: result.content.responseData
-            });
-        }
-    });
+  service.getAll(pageNumber, function (result) {
+    if(result.err || !result.content.result){
+      res.json({
+        err: true,
+        msg: result.msg
+      });
+    }else{
+      res.json({
+        err: false,
+        msg: result.content.responseMessage,
+        statusList: result.content.responseData
+      });
+    }
+  });
 });
 
 router.get('/checkAdministrator', function (req, res, next) {
-    var service = new commonService.CommonService('checkAdministratorName');
-    var parameter = req.query.administratorName;
+  var service = new commonService.CommonService('checkAdministratorName');
+  var parameter = req.query.administratorName;
 
-    service.get(parameter, function (result) {
-        if(result.err || !result.content.result){
-            res.json({
-                err: true,
-                msg: result.msg
-            });
-        }else{
-            res.json({
-                err: false,
-                msg: result.content.responseMessage,
-                exist: result.content.responseData
-            });
-        }
-    });
+  service.get(parameter, function (result) {
+    if(result.err || !result.content.result){
+      res.json({
+        err: true,
+        msg: result.msg
+      });
+    }else{
+      res.json({
+        err: false,
+        msg: result.content.responseMessage,
+        exist: result.content.responseData
+      });
+    }
+  });
 });
 
 router.post('/', function (req, res, next) {
-    var service = new commonService.CommonService('administrator');
-    var data = {
-        administratorID: req.body.administratorID,
-        administratorName: req.body.administratorName,
-        account: req.body.account,
-        cellphone: req.body.cellphone,
-        email: req.body.email,
-        customerRole: req.body.customerRole,
-        status: req.body.status,
-        loginUser: req.body.loginUser
-    };
+  var service = new commonService.CommonService('administrator');
+  var data = {
+    administratorID: req.body.administratorID,
+    administratorName: req.body.administratorName,
+    account: req.body.account,
+    cellphone: req.body.cellphone,
+    email: req.body.email,
+    customerRole: req.body.customerRole,
+    status: req.body.status,
+    loginUser: req.body.loginUser
+  };
 
-    service.add(data, function (result) {
-        if(result.err){
-            res.json({
-                err: true,
-                msg: result.msg
-            });
-        }else{
-            res.json({
-                err: false,
-                data: result.content
-            });
-        }
-    });
+  service.add(data, function (result) {
+    if(result.err){
+      res.json({
+        err: true,
+        msg: result.msg
+      });
+    }else{
+      res.json({
+        err: false,
+        data: result.content
+      });
+    }
+  });
 });
 
 router.put('/', function(req,res,next){
-    var service = new commonService.CommonService('administrator');
-    var data = {
-        administratorID: req.body.administratorID,
-        administratorName: req.body.administratorName,
-        account: req.body.account,
-        cellphone: req.body.cellphone,
-        email: req.body.email,
-        customerRole: req.body.customerRole,
-        status: req.body.status,
-        loginUser: req.body.loginUser
-    };
+  var service = new commonService.CommonService('administratorApprove');
+  var data = {
+    administratorID: req.body.administratorID,
+    customerRole: req.body.customerRole,
+    status: req.body.status,
+    loginUser: req.body.loginUser
+  };
 
-    service.change(data, function (result) {
-        if(result.err){
-            res.json({
-                err: true,
-                msg: result.msg
-            });
-        }else{
-            res.json({
-                err: false,
-                data: result.content
-            });
-        }
-    });
+  service.change(data, function (result) {
+    if(result.err){
+      res.json({
+        err: true,
+        msg: result.msg
+      });
+    }else{
+      res.json({
+        err: false,
+        data: result.content
+      });
+    }
+  });
 });
 
 router.delete('/', function (req, res, next) {
-    var service = new commonService.CommonService('administrator');
-    var administratorID = req.query.administratorID;
+  var service = new commonService.CommonService('administrator');
+  var administratorID = req.query.administratorID;
 
-    service.delete(administratorID, function (result) {
-        if(result.err){
-            res.json({
-                err: true,
-                msg: result.msg
-            });
-        }else{
-            res.json({
-                err: false,
-                data: result.content
-            });
-        }
-    });
+  service.delete(administratorID, function (result) {
+    if(result.err){
+      res.json({
+        err: true,
+        msg: result.msg
+      });
+    }else{
+      res.json({
+        err: false,
+        data: result.content
+      });
+    }
+  });
 });
 
 module.exports = router;
